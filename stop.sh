96924c9d124c3747cc87c0f3682e2d701ad4301c
wys899195 <73873365+wys899195@users.noreply.github.com>
2024-04-27 23:00:27 +0800

    feat: Change service name

--- stop.sh
+++ stop.sh
@@ -1,5 +1,5 @@
-kubectl -n library-system delete -f library-system-backend.yaml
-kubectl -n library-system delete -f library-system-frontend.yaml
+kubectl -n library-system delete -f library-system-core.yaml
+kubectl -n library-system delete -f library-system.yaml
 
 kubectl -n library-system delete configmap mysql-initdb --from-file library_system.sql
 kubectl -n library-system delete -f library-system-mysql.yaml
